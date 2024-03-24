17a6271778af2c33043b019be1b2aa5f1f14836a
Meral Cihangirli <mcihangirli@gmail.com>
2024-03-24 20:07:43 +0300

    Css Kisimlari

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,4 +1,4 @@
-
+import 'bulma/css/bulma.css'
 import './App.css';
 import Course from './Course';
 import Angular from './images/angular.jpg'
@@ -9,18 +9,32 @@ import KompleWeb from './images/kompleweb.jpg'
 function App() {
   return (
     <div className="App">
-      {/* <img src={Angular} alt=""/> */}
-      <Course image={Angular} title="Angular" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+         
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+            <Course image={Angular} title="Angular" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+          </div>
+          <div className='column'>
+            <Course image={Bootstrap} title="Bootstrap5" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+          </div>
+          <div className='column'>
+            <Course image={Csharp} title="Csharp" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+          </div>
+          <div className='column'>
+            <Course image={KompleWeb} title="KompleWeb" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+          </div>
+        </div>
+        </section>
+       
+      </div>
 
-      
-      <Course image={Bootstrap} title="Bootstrap5" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-
-     
-      <Course image={Csharp} title="Csharp" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-
-      
-      <Course image={KompleWeb} title="KompleWeb" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-      
     </div>
   );
 }
--- cards/src/Course.js
+++ cards/src/Course.js
@@ -11,17 +11,29 @@ import React from 'react'
 
 //ffc
 function Course({image,title,description}) {
-    //console.log(props);
-    //const title=props.title
-    //const description=props.description
-    //const {title,description}=props;
-
+    
     return (
-        <div>
-            <img src={image} alt=""/>
-            <div>{title}</div>
-            <div>{description}</div>
-        </div>
+        <div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img src={image} alt="Kurslarim"/>
+    </figure>
+  </div>
+  <div className="card-content">
+    <div className="media">
+           <div className="media-content">
+        <p className="title is-4">{title}</p>
+          </div>
+    </div>
+
+    <div className="content">{description}</div>
+  </div>
+</div>
+        // <div>
+        //     <img src={image} alt=""/>
+        //     <div>{title}</div>
+        //     <div>{description}</div>
+        // </div>
 
     );
 }
