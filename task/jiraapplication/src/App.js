84e4dd0d691f84d40778e1c97942bae752a9b166
Meral Cihangirli <mcihangirli@gmail.com>
2024-03-31 02:17:08 +0300

    Task Formu Olusturma ve Css Duzenlemesi

---
+++ task/jiraapplication/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TaskCreate from './components/TaskCreate';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskCreate/>
+      <h1>Görevler</h1>
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
