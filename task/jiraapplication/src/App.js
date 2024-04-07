beae72c4d2683240535d8f067b4d7f60007ae102
Meral Cihangirli <mcihangirli@gmail.com>
2024-04-07 23:02:09 +0300

    Task projesine useEffect ekleme - axios ile get-post-delete-put işlemleri

--- task/jiraapplication/src/App.js
+++ task/jiraapplication/src/App.js
@@ -2,34 +2,55 @@
 import './App.css';
 import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [tasks, setTasks] = useState([])
-  const createTask = (title, taskDesc) => {
+  const createTask = async (title, taskDesc) => {
+
+    const response = await axios.post('http://localhost:3000/tasks', {
+      title,
+      taskDesc
+    });
+    console.log(response);
     const createdTasks = [
-      ...tasks, {
-        id: Math.round(Math.random() * 999999),
-        title: title,
-        taskDesc: taskDesc
-      }
+      ...tasks,
+      response.data
+
     ];
     setTasks(createdTasks);
   };
 
-  const deleteTaskById = (id) => {
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3000/tasks/');
+
+    setTasks(response.data)
+  }
+
+  useEffect(() => {
+    fetchTasks();
+
+  }, [])
+
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`);
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     })
     setTasks(afterDeletingTasks);
   };
 
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc,
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
       }
-return task;
+      return task;
     })
     setTasks(updatedTasks);
   };
