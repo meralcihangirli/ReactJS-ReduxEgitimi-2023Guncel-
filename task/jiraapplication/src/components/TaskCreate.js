68b0231c2539373cb716919cbd38efe278abb1ed
Meral Cihangirli <mcihangirli@gmail.com>
2024-04-08 15:47:46 +0300

    Task Projesinde Propslarla kurulan yapıyı useContext kullanarak değiştirme

--- task/jiraapplication/src/components/TaskCreate.js
+++ task/jiraapplication/src/components/TaskCreate.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import {useContext } from 'react';
+import TasksContext from '../context/task';
 
-function TaskCreate({ onCreate, task, taskFormUpdate, onUpdate }) {
+function TaskCreate({ task, taskFormUpdate,onUpdate}) {
+    const {createTask}=useContext(TasksContext)
     const [title, setTitle] = useState(task ? task.title : '')
     const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : '')
 
@@ -17,9 +20,11 @@ function TaskCreate({ onCreate, task, taskFormUpdate, onUpdate }) {
         event.preventDefault();
         if (taskFormUpdate) {
             onUpdate(task.id, title, taskDesc)
+        //  editTaskById(task.id, title, taskDesc)
         }
         else {
-            onCreate(title, taskDesc)
+            //onCreate(title, taskDesc)
+            createTask(title,taskDesc)
         }
 
         setTitle('');
--- task/jiraapplication/src/components/TaskShow.js
+++ task/jiraapplication/src/components/TaskShow.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 import TaskCreate from "./TaskCreate";
+import {useContext } from 'react';
+import TasksContext from '../context/task';
 
-function TaskShow({ task, onDelete,onUpdate }) {
+function TaskShow({ task}) {
+    const {deleteTaskById,editTaskById}=useContext(TasksContext)
     const [showEdit, setShowEdit] = useState(false)
 
     const handleDeleteClick = () => {
-        onDelete(task.id)
+        //onDelete(task.id)
+        deleteTaskById(task.id);
     };
     const handleEditClick = () => {
         setShowEdit(!showEdit);
     };
     const handleSubmit = (id,updatedTitle,updatedTaskDesc) => {
         setShowEdit(false);
-        onUpdate(id,updatedTitle,updatedTaskDesc);
+       // onUpdate(id,updatedTitle,updatedTaskDesc);
+       editTaskById(id,updatedTitle,updatedTaskDesc);
     };
 
 
     console.log(task)
     return (<div className="task-show">
-        {showEdit ? (<TaskCreate task={task} taskFormUpdate={true} onUpdate={handleSubmit} />) : (
+        {showEdit ? (<TaskCreate task={task} taskFormUpdate={true} onUpdate={handleSubmit}  />) : (
             <div>
                 <h3 className="task-title">Göreviniz</h3>
                 <p>{task.title}</p>
