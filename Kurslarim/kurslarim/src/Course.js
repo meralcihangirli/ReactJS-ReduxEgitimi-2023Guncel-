9ed2eee80c3b51ed562d2a63abe10f15a26a5ae3
Meral Cihangirli <mcihangirli@gmail.com>
2024-04-15 17:41:53 +0300

    Kurslarim projesini tekli kart yapısına çevirmek ve önce-sonra butonlarıyla kurslar arası geçiş

--- Kurslarim/kurslarim/src/Course.js
+++ Kurslarim/kurslarim/src/Course.js
@@ -1,15 +1,10 @@
-function Course({ id, content, title, price,removeOneCourse }) {
+function Course({ id, content, title, price, removeOneCourse }) {
     console.log(id);
     console.log(content);
 
-    return <div className="card">
-        <div className="cardTitlePrice">
-            <h2 className="cardTitle">{title}</h2>
-            <h4 className="cardPrice">{price}TL</h4>
-            <p className="cardContent">{content}</p>
-        </div>
-        <button className="cardDeleteBtn" onClick={()=>removeOneCourse(id)}>Sil</button>
-    </div>;
+    return 
+    
+    
 }
 
 export default Course;
--- Kurslarim/kurslarim/src/Courses.js
+++ Kurslarim/kurslarim/src/Courses.js
@@ -1,20 +1,55 @@
 import Course from "./Course";
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
-function Courses({ courses,removeCourse }) {
-    //console.log(courses);
+function Courses({ courses, removeCourse }) {
+    const [index, setIndex] = useState(0)
+    const { content, title, price } = courses[index]
+
+    const checkIndex = (index) => {
+        if (index < 0) {
+            return courses.length - 1;
+        }
+        if(index>courses.length-1){
+            return 0;
+        }
+        return index;
+    }
+
+    const prevCourse = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkIndex(newIndex)
+        })
+    };
+
+    const nextCourse = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkIndex(newIndex)
+        })};
+
     return <div className="courseMainDiv">
         <div>
             <h2>Kurslarım</h2>
         </div>
         <div className="cardDiv">
-            {
-                courses.map((course) => {
-                    return (
-                        <Course key={course.id} {...course} removeOneCourse={removeCourse}/>
-                       // course={course}
-                    )
-                }) 
-            }
+            <button className="prevNextBtn" onClick={prevCourse}>
+                <FaChevronLeft />
+            </button>
+            <div className="card">
+                <div className="cardTitlePrice">
+                    <h2 className="cardTitle">{title}</h2>
+                    <h4 className="cardPrice">{price}TL</h4>
+                    <p className="cardContent">{content}</p>
+                </div>
+                <p>{content}</p>
+
+            </div>
+            <button className="prevNextBtn" onClick={nextCourse}>
+                <FaChevronRight />
+            </button>
+
         </div>
     </div>;
 }
