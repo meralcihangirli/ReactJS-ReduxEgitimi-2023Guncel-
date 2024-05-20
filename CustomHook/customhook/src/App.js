28aa61a5512abd7ee78d75f3180a217234461661
Meral Cihangirli <mcihangirli@gmail.com>
2024-05-20 21:07:34 +0300

    Custom Hook Form Örneği
    Custom Hook Form Örneği

--- CustomHook/customhook/src/App.js
+++ CustomHook/customhook/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import TitleOne from './TitleOne';
 import Counter from './Counter';
+import PersonalInfo from './PersonalInfo';
 
 
 function App() {
   return (
     <div className="App">
      {/* <TitleOne/> */}
-     <Counter/>
-
+     {/* <Counter/> */}
+<PersonalInfo/>
     </div>
   );
 }
