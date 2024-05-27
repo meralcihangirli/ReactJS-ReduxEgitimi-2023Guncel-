de5106192a145595196989bd5d37966753555e17
Meral Cihangirli <mcihangirli@gmail.com>
2024-05-27 19:20:15 +0300

    Burger Projesi-Navbar Bölümü
    Burger Projesi-Navbar Bölümü

---
+++ BurgerProject/burgerproject/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home'
+import { Menu } from './components/Menu';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import {Navbar} from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/menu' exact element={<Menu />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/contact' exact element={<Contact />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
