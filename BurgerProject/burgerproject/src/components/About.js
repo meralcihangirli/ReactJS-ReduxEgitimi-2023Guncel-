d0c15b9c1892a2d8432c2e9cd5dd997a84e0ab54
Meral Cihangirli <mcihangirli@gmail.com>
2024-05-30 18:09:41 +0300

    Burger Projesi-Hakkımızda ve İletişim Sayfası
    Burger Projesi-Hakkımızda ve İletişim Sayfası

--- BurgerProject/burgerproject/src/components/About.js
+++ BurgerProject/burgerproject/src/components/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import BannerImage from '../assets/5.png'
+import '../styles/About..css'
 
 export const About = () => {
     return (
-        <div>
-            About
+        <div className='about'>
+            <div className='aboutTop' style={{backgroundImage:`url(${BannerImage})`}}></div>
+            <div className='aboutBottom'>
+            <h1>Hakkımızda</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
         </div>
-    )
-}
+    );
+};
--- BurgerProject/burgerproject/src/components/Contact.js
+++ BurgerProject/burgerproject/src/components/Contact.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import BannerImage from '../assets/logo.png'
+import '../styles/Contact.css'
 
 export const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className='contact'>
+            <div className='leftSide' style={{ backgroundImage: `url(${BannerImage})` }}>
+            </div>
+            <div className='rightSide'>
+                <h1>Bizimle İletişime Geçebilirsiniz</h1>
+                <form>
+                    <label>Adınız Soyadınız</label>
+                    <input type="text" name='name' placeholder='Lütfen Adınız Soyadınız alanını doldurunuz.'></input>
+                    <label>Email</label>
+                    <input type="email" name='email' placeholder='Lütfen Mail Adresinizi bu alana giriniz.'></input>
+                    <label>Mesajınız</label>
+                    <textarea rows="6" name='message' placeholder='Mesajınızı iletebilirsiniz.'></textarea>
+                    <button>Gönder</button>
+                </form>
+            </div>
         </div>
     )
 }
